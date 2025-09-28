ecbc47429ed65c6cdd889aa6d94f66d3529a485a
Вайндрах Елизавета <liza_vaindrakh@mail.ru>
2025-09-28 17:49:36 +0300

    feat(types/practice-2): имя Лиза через Unicode

---
+++ 02-types-and-operators/01-basic-types-and-literals/practice-2/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main(){
+    char l = '\u041B';
+    char i = '\u0438';
+    char z = '\u0437';
+    char a = '\u0430';
+    System.out.print("" + l + i + z + a);
+}

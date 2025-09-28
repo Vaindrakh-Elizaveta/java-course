2944b83f5836d6b761ccbeb07e1a43edddcef928
Вайндрах Елизавета <liza_vaindrakh@mail.ru>
2025-09-28 18:22:31 +0300

    Практика 4

---
+++ 02-types-and-operators/01-basic-types-and-literals/practice-4/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    String name = "Лиза";
+    int age = 20;
+    double grade = 4.7;
+    boolean student = true;
+
+    // Перегрузка + со String
+    System.out.println("Имя: " + name);
+    System.out.println("Возраст: " + age);
+    System.out.println("Средняя оценка: " + grade);
+    System.out.println("Студент: " + student);
+}

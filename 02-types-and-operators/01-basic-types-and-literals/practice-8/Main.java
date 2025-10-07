10d8e3f5c14f64723ffdf9adb227f9cd352a9a8d
Вайндрах Елизавета <liza_vaindrakh@mail.ru>
2025-10-07 13:42:20 +0300

    practice-8

---
+++ 02-types-and-operators/01-basic-types-and-literals/practice-8/Main.java
@@ -0,0 +1,9 @@
+void main(){
+    var number = 10; // тип int
+    var name = "Лиза"; // тип String
+    var pi = 3.14159;  // тип double
+
+    System.out.println(number); // 10
+    System.out.println(name); // Лиза
+    System.out.println(pi);  // 3.14159
+}

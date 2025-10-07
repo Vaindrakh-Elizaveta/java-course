e1c626ed1066ab0c86c626f9b4617be6b43f211e
Вайндрах Елизавета <liza_vaindrakh@mail.ru>
2025-10-07 11:56:51 +0300

    types\practice-2

---
+++ 02-types-and-operators/01-basic-types-and-literals/practice-5/Main.java
@@ -0,0 +1,21 @@
+void main(){
+    int a = 5;
+    double b = 3.5;
+    double result = a + b; //int --> double
+    System.out.println("Результат (int + double): " + result);
+
+    byte x = 10;
+    short y = 20;
+    int sum = x + y; //оба приводятся к int
+    System.out.println("Результат (byte + short): " + sum);
+
+    float f = 3.5f;
+    long l = 2L;
+    float res = f + l; //Long --> float
+    System.out.println("Результат (float + long): " + res);
+
+    char c = 'A'; //код символа 65
+    int n = 5;
+    result = c + n; //оба операнда становятся int
+    System.out.println("Результат (char + int): " + result);
+}
